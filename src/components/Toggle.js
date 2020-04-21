d6c6b3ec13b16e2046b37c01fb5ff4ae28e76468
Carolyn Nickell <caro.lovin.it@gmail.com>
2020-04-21 15:59:33 -0700

    added icons for theme switcher, inside a button for now. Will update for switch soon

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { func, string } from 'prop-types';
 import styled from 'styled-components';
-import styles from './Toggle.styled.js';
+// import styles from './Toggle.styled.js';
 
 const Toggle = ({ theme, toggleTheme }) => {
   const isLight = theme === 'light';
   return (
+    <>
     <button className="togglebutton" onClick={toggleTheme}>
-        Toggle
+      <i className="sunIcon" className="fas fa-sun"></i><i className="moonIcon" className="fas fa-moon"></i>
+
     </button>
+    <div className="toggleIconDiv" onClick={toggleTheme}>
+      {/* <i className="sunIcon" className="fas fa-sun"></i><i className="moonIcon" className="fas fa-moon"></i> */}
+      </div>
+    </>
   );
 };
 
