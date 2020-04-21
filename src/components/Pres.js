07d37e7262ba3909d3f922e603a1f03be5aa28fd
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-04-21 12:40:32 -0700

    rendered sections with talking points

--- src/components/Pres.js
+++ src/components/Pres.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Points from './Points'
 
 function Pres(props) {
     console.log('Pres-props', props)
@@ -6,13 +7,23 @@ function Pres(props) {
         return <></>
     }
 
-  
+    const renderSections = props.presentation.sections.map((section, index) => {
+        return (
+            <div>
+                <h3>{section.title} {section.time}</h3>
+                <ul>
+                    <Points points={section.talking_points} />
+                </ul>
+            </div>
+        )
+    })
          
             
     return (
         <>
             <h1>Pres Page</h1>
             <h2>Title: {props.presentation.name}</h2>
+            {renderSections}
         </>
     )
 }
