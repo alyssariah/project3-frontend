c96fe3c3765453eb7ca7e277031de42c40e92c62
marshall brock <marshallbrock@gmail.com>
2020-04-21 11:59:01 -0500

    passed props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from "react"
 import { getAllPres} from '../services/api-helper'
 import '../css/home.css'
-//createPres, deletePres, updatePres
-function Home(){
+import { Link} from 'react-router-dom';
+
+function Home(props){
 
     const [pres, setPres] = useState([])
     const [isLoading, setIsloading] = useState(true)
+
     
+  
 
     useEffect(() => {
         const makeAPICall = async () => {
@@ -17,18 +20,30 @@ function Home(){
         makeAPICall()
       }, [])
 
+      const BlastOff =(presentation)=>{
+        console.log("houston, all blue skies from here", presentation)
+        
+         props.clickPresentation(presentation)
+      }
+
     const renderPres = pres.map( (pres, index) => {
         return (
-          <div key={index} className="presList">
+          <div key={index} className="presList" onClick ={()=>BlastOff(pres)}>
            <h2>{pres.name}</h2>
           </div> 
           )
       })
-
+      
 
   return (
+
     <div className="main">
       {!isLoading && <div className="presentationsGroup">{renderPres}</div>}
+      <button><Link to ="/new">New Presentation</Link></button>
+      
+      
+      
     </div>
+    
 )}
 export default Home
