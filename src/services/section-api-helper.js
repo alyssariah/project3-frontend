5177ffc7169a687d32c42c8ab2f3681b9622c688
marshall brock <marshallbrock@gmail.com>
2020-04-20 20:19:47 -0500

    added helpers

--- src/services/section-api-helper.js
+++ src/services/section-api-helper.js
@@ -5,15 +5,15 @@ const api = axios.create({
 });
 
 
-
-//create sections
+export const getAllSect = async ()=>{
+    const resp =await api.get(`sections/`)
+    return resp.data
+}
 
 export const findById = async (sectId)=>{
     const resp =await api.get(`sections/${sectId}`)
     return resp.data
 }
-
-
 
 export const createSect = async (presId, body) => {
     const resp = await api.post(`/sections/${presId}`, body);
@@ -21,13 +21,25 @@ export const createSect = async (presId, body) => {
 }
 
 //update
-export const updateSect = async (presId, sectId, body) => {
-    const resp = await api.put(`/sections/${presId}/${sectId}`, body);
+export const updateSect = async ( sectId, body) => {
+    const resp = await api.put(`/sections/${sectId}`, body);
     return resp.data
 }
 
 //delete
-export const deleteSect = async (presId, sectId) => {
+export const deleteSect = async ( sectId) => {
+    const resp = await api.delete(`/sections/${sectId}`);
+    return resp.data
+};
+
+export const deleteAllSect = async (presId, sectId) => {
     const resp = await api.delete(`/sections/${presId}/${sectId}`);
     return resp.data
 };
+
+// router.get('/', sectionController.getAll)
+// router.get('/:sectId', sectionController.getById)
+// router.post('/:presId', sectionController.create)
+// router.put('/:sectId', sectionController.update)
+// router.delete('/:sectId', sectionController.remove)
+// router.delete('/', sectionController.removeAll)
