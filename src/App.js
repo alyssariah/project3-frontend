ad907fa19ceea7bbbb7a646154fbc447d1f096c7
Carolyn Nickell <caro.lovin.it@gmail.com>
2020-04-20 19:10:54 -0700

    dark/light toggle theme with styled-components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './components/theme';
+import { GlobalStyles } from './components/global';
+import Toggle from './components/Toggle';
+
+
 import Home from './components/Home';
 // import Pres from './components/Pres';
 import NewPres from './components/NewPres';
@@ -8,9 +15,29 @@ import "./App.css"
 import { Link, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    // if the theme is not light, then set it to dark
+    if (theme === 'light') {
+      setTheme('dark');
+    // otherwise, it should be light
+    } else {
+      setTheme('light');
+    }
+  }
+
   return (
+    
     <>
     <header><Link to ="/"><h1>Presentation Timer</h1></Link><div>i</div></header>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}> 
+         <>
+        <GlobalStyles />
+        {/* <button onClick={toggleTheme}>Toggle theme</button> */}
+        <Toggle theme={theme} toggleTheme={toggleTheme} />
+        <h1>It's a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
+        </>
+    </ThemeProvider>
     <Switch>
 	      <Route exact path="/" component={Home}/>
 	      {/* <Route exact path="/pres" component={Pres}/> */}
@@ -18,6 +45,7 @@ function App() {
         {/* <Route exact path="/update" component={UpdatePres}/> */}
        <Redirect to="/"/>
     </Switch>
+
     </>
   );
 }
