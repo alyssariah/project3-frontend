9accb4476c2122ad73dbcc61092bbeaea7e97ad6
Alyssa Jackson <alyssajackson@rescomp-19-388097.stanford.edu>
2020-04-21 17:43:14 -0700

    working on editing material

--- src/components/UpdatePres.js
+++ src/components/UpdatePres.js
@@ -1,34 +1,31 @@
 import React, {useState} from 'react'
-import Points from './Points'
 import '../css/pres.css'
-import {Link} from "react-router-dom"
+import UpdateSect from './UpdateSect'
 
-function Pres(props) {
+
+function UpdatePres(props) {
     let totalTime = 0
     console.log('Pres-props', props)
     if(!props.presentation){
         return <></>
     }
 
+
     const renderSections = props.presentation.sections.map((section, index) => {
         totalTime += section.time
         return (
-            <div>
-                <h3>Section {index +1}: {section.title} <span className="timeDisplay">{section.time}</span></h3>
-                <Points points={section.talking_points} />
-            </div>
+           <UpdateSect section={section} inedx={index}/>
         )
     })
          
             
     return (
-        <div className="presMain">
-            <Link to='/update'><i class="far fa-edit"></i></Link>
-            <h2>Title: {props.presentation.name}</h2>
+        <div className="updatePresMain">
+            <h2><i class="far fa-edit"></i>{props.presentation.name}</h2>
             {renderSections}
             <p className="time">Total time: {totalTime}</p>
         </div>
     )
 }
 
-export default Pres
+export default UpdatePres
