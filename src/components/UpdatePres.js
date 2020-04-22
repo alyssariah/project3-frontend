12e1490da4456387aa97db0b4e842772ea4ef8ba
Carolyn Nickell <caro.lovin.it@gmail.com>
2020-04-22 08:27:50 -0700

    updated UpdatePres

--- src/components/UpdatePres.js
+++ src/components/UpdatePres.js
@@ -2,14 +2,28 @@ import React, {useState} from 'react'
 import '../css/pres.css'
 import UpdateSect from './UpdateSect'
 
-
-function UpdatePres(props) {
+function UpdatePres(props) {   
+    const [name, setName] = useState('') 
+    const [showEdit, setShowEdit] = useState(false)
+    
     let totalTime = 0
+
     console.log('Pres-props', props)
+
     if(!props.presentation){
         return <></>
     }
+    
+    const nameChange = (e) => {
+        setName(e.target.value)
+    }
 
+    const presNameSubmit = async(e) => {
+        e.preventDefault()
+        const json = await UpdatePres(props.pres._id, {"name": name})
+        setShowEdit(false)
+        // getSections()
+    }
 
     const renderSections = props.presentation.sections.map((section, index) => {
         totalTime += section.time
@@ -17,12 +31,17 @@ function UpdatePres(props) {
            <UpdateSect section={section} inedx={index}/>
         )
     })
-         
             
     return (
         <div className="updatePresMain">
-            <h2><i class="far fa-edit"></i>{props.presentation.name}</h2>
-            {renderSections}
+            <h2><i onClick= {()=>setShowEdit(!showEdit)} class="far fa-edit"></i>{props.presentation.name}</h2>
+            {
+                showEdit &&<form className="nameForm" onSubmit={presNameSubmit}>
+                <p><label>Presentation Name: </label><input type="text"  value={name} onChange={nameChange} required="required"/></p>
+                <button>update</button>
+                </form>
+             }
+        {renderSections}
             <p className="time">Total time: {totalTime}</p>
         </div>
     )
