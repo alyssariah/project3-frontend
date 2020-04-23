fbd674ceb242ca0860f81d49e0491bb12b677bfe
Carolyn Nickell <caro.lovin.it@gmail.com>
2020-04-23 10:30:44 -0700

    updated instructions

--- src/App.js
+++ src/App.js
@@ -37,14 +37,22 @@ function App() {
   }
 
   return (
-    
     <>
-
     <header><Link to ="/"><h1><i class="fas fa-home"></i>Presentation <span>Buddy</span></h1></Link><i onClick={() => setShowInstructions(!showInstructions)} className="fas fa-info-circle"></i></header>
     {showInstructions && 
       <div className="instructions">
-        <h3>Instructions</h3>
-      </div>}
+        <ul>
+        <span className="closeInstructions" onClick={() => setShowInstructions(!showInstructions)}><i className="fas fa-times-circle"></i></span> 
+            <h4>Instructions</h4>
+            <p><strong>Presentation Buddy helps you plan your presentation and stay on track.</strong></p>
+              <ol>
+                <li>Create and name your new presentation, or open a saved one / example.</li>
+                <li>Add to and edit your presentation with sections, estimated time per section, and talking points.</li>
+                <li>You're ready! As the timer counts down, it turns <span className="redtext"><strong>red</strong></span> when it’s time for the next section. Pause or skip sections along the way.</li>
+              </ol>
+            <p>Designed and built by <a href="https://www.linkedin.com/in/alyssa-riah-jackson/" target="_blank">Alyssa Jackson</a>, <a href="https://www.linkedin.com/in/stanleypascua/" target="_blank">Stanley Pascua</a>, <a href="https://www.linkedin.com/in/marshallbrock/" target="_blank">Marshall Brock</a>, and <a href="https://www.linkedin.com/in/carolynnickell/" target="_blank">Carolyn Nickell</a> through General Assembly.</p>
+          </ul>
+        </div>}
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}> 
          <>
         <GlobalStyles />
